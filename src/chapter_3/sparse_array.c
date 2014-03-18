17ee1cc77e4aff326a23c59f00bbc708f1bd1337
Brandon Koepke <bdkoepke@gmail.com>
2014-03-17 22:41:03 -0700

    sparse array template

---
+++ src/chapter_3/sparse_array.c
@@ -0,0 +1,48 @@
+#include "array_container.h"
+#include "contract.h"
+#include "type.h"
+
+#include <stdlib.h>
+
+typedef struct {
+  container_vtable *vtable;
+	size_t n;
+} SparseArray;
+
+static void sparse_array_free(Object *o) {
+  free(o);
+}
+
+static void sparse_array_insert(Container *c, void *x) {
+  SparseArray *s = (SparseArray *)c;
+  size_t _x = POINTER_TO_INT(x);
+  contract_requires(x != NULL && container_search(c, x) ==
+                                     INT_TO_POINTER(false) && _x <= s->n);
+}
+
+static void *sparse_array_search(const Container *c, const void *x) {
+  SparseArray *s = (SparseArray *)c;
+  size_t _x = POINTER_TO_INT(x);
+}
+
+static void sparse_array_delete(Container *c, const void *x) {
+  SparseArray *s = (SparseArray *)c;
+  size_t _x = POINTER_TO_INT(x);
+  contract_requires(x != NULL && _x <= s->n);
+}
+
+static bool sparse_array_empty(const Container *c) {
+}
+
+Container *sparse_array_new(size_t n) {
+  static container_vtable vtable = {
+    {.free = sparse_array_free },
+        .insert = sparse_array_insert, .search = sparse_array_search,
+        .delete = sparse_array_delete, .empty = sparse_array_empty
+  };
+
+  SparseArray *s = malloc(sizeof(SparseArray));
+	s->vtable = &vtable;
+	s->n = n;
+  return (Container *)s;
+}
---
+++ src/chapter_3/sparse_array.h
@@ -0,0 +1,10 @@
+#ifndef SPARSE_ARRAY_H
+#define SPARSE_ARRAY_H
+
+#include "container.h"
+
+#include <stddef.h>
+
+Container *sparse_array_new(size_t);
+
+#endif /* SPARSE_ARRAY_H */
