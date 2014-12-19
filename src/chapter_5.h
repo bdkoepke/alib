b617403981c13936763aaec97cc92f25a8523bdb
Charlie Root <root@clang.swordfish.pw>
2014-12-19 20:02:55 +0000

    Initial conversion to noweb

--- src/chapter_5.h
+++
@@ -1,7 +0,0 @@
-#ifndef CHAPTER_5_H
-#define CHAPTER_5_H
-
-void test_linked_graph(void);
-void test_matrix_graph(void);
-
-#endif /* CHAPTER_5_H */
--- src/lang/class.c
+++
@@ -1,3 +0,0 @@
-#include "class.h"
-
-const char *class_name(const Class *c) { return c->name; }
